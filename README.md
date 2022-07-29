f4fbb4ea438a5306ba36b2504dd5f3b4e7f3c5b1
Rustam Ziyadov <zyderus@gmail.com>
2022-07-30 00:58:50 +0500

    Initialize project using Create React App

---
+++ README.md
@@ -0,0 +1,9 @@
+# COUNTDOWN TIMER TO 2023
+
+### Install
+
+`pnpm install`
+
+### Run
+
+`pnpm start`
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Timer from "./Timer";
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='app'>
+        <div className='container'>
+          <h1 className='header'>2023 in</h1>
+          <Timer />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
